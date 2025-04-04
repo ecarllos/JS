eae34dbe8a0abb0b6a8a45fbbd46ca4b93da58df
Eduardo <ecarllos017@gmail.com>
2025-04-04 14:01:29 -0300

    tratamento de dados (String - Numbers)

--- curso JS/01.js
+++ curso JS/01.js
@@ -1,2 +1,29 @@
-var nome = prompt("qual é o seu nome?")
-alert(`Bem vindo(a) ${nome}`)
+// EXERCICIO 01
+
+// var nome = prompt("qual é o seu nome?")
+// alert(`Bem vindo(a) ${nome}`)
+
+
+
+// EXERCICIO 02
+
+
+// var n1 = parseInt(prompt("digite um número"))
+// var n2 = parseInt(prompt("digite um número"))
+// var s = n1 + n2
+
+// alert(`a soma dos números é  ${s}`)
+
+
+
+// EXERCICIO 03
+
+// const nome = prompt("Digite seu nome: ")
+
+// document.writeln(`Seja bem vindo ${nome} <br>`)
+// document.writeln(`Seu nome possui ${nome.length} letras <br>`)                 //contar 
+// document.writeln(`Seu nome em maiúsculo fica ${nome.toUpperCase()} <br>`)       //maiusculo
+// document.writeln(`Seu nome em minúsculo fica ${nome.toLowerCase()} <br>`)      //minusculo
+
+
+
