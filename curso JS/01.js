f6b23187ed20debb180c97b16667badd799a869f
Eduardo <ecarllos017@gmail.com>
2025-04-04 14:12:56 -0300

    Tratamento number (casas decimais, cotações de dinheiro)

--- curso JS/01.js
+++ curso JS/01.js
@@ -21,9 +21,16 @@
 // const nome = prompt("Digite seu nome: ")
 
 // document.writeln(`Seja bem vindo ${nome} <br>`)
-// document.writeln(`Seu nome possui ${nome.length} letras <br>`)                 //contar 
+// document.writeln(`Seu nome possui ${nome.length} letras <br>`)                   //contar 
 // document.writeln(`Seu nome em maiúsculo fica ${nome.toUpperCase()} <br>`)       //maiusculo
 // document.writeln(`Seu nome em minúsculo fica ${nome.toLowerCase()} <br>`)      //minusculo
 
 
 
+// EXERCICIO 04
+
+// var n1 = 2145.7
+
+// alert(n1.toFixed(2))                                                          //aumentar as casas decimais
+// alert(n1.toFixed(2)).replace('.' , ',')                                      //mudar o ponto "." para vírgula ","
+// alert(n1.toLocaleString('pt-BR' , {style: 'currency' , currency: 'BRL'}))   //Transformar número em DINHEIRO
