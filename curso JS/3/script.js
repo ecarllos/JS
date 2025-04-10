90c93f7ed9aaa7afeb5b4317eb52be74932e8384
Eduardo <ecarllos017@gmail.com>
2025-04-10 11:53:06 -0300

    estudo de IF & ELSE

---
+++ curso JS/3/script.js
@@ -0,0 +1,9 @@
+const vel = 75.5
+
+console.log(`sua velicidade é de ${vel} Km/h`)
+if (vel > 60){
+    console.log(`Você ultrapassou o limite de velocidade de 60 Km/h`)
+}
+else{
+    console.log(`Você está respeitando o limite de velocidade de 60 Km/h`)
+}
