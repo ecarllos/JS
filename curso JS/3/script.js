77bb92bf65862d3cfdef0814e910691832e57d52
Eduardo <ecarllos017@gmail.com>
2025-04-14 17:22:09 -0300

    repaginacao

--- curso JS/3/script.js
+++ curso JS/3/script.js
@@ -1,9 +1,31 @@
-const vel = 75.5
+// EXERCICIO 01
 
-console.log(`sua velicidade é de ${vel} Km/h`)
-if (vel > 60){
-    console.log(`Você ultrapassou o limite de velocidade de 60 Km/h`)
+// const vel = 75.5
+
+// console.log(`sua velicidade é de ${vel} Km/h`)
+// if (vel > 60){
+//     console.log(`Você ultrapassou o limite de velocidade de 60 Km/h`)
+// }
+// else{
+//     console.log(`Você está respeitando o limite de velocidade de 60 Km/h`)
+// }
+
+
+
+// EXERCICIO 02 
+
+console.log("(BR, EUA, RUS, POR)")
+var pais = console.log("Digite seu País: ")
+
+if (pais == "BR"){
+    console.log("Você é Brasileiro(a)")
 }
-else{
-    console.log(`Você está respeitando o limite de velocidade de 60 Km/h`)
+else if(pais == "EUA"){
+    console.log("Você é Americano(a)")
+}
+else if(pais == "RUS"){
+    console.log("Você é Russo(a)")
+}
+else if(POR){
+    console.log("Você é Português")
 }
