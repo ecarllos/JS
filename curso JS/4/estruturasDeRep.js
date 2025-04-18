0c0c154d3d9793034c303c2fade614ba70a3643c
Eduardo <ecarllos017@gmail.com>
2025-04-18 18:30:06 -0300

    estrutura de rep

---
+++ curso JS/4/estruturasDeRep.js
@@ -0,0 +1,27 @@
+// ESTRUTURAS DE REPETICAO
+
+
+// com teste lógico no inicio (WHILE)
+
+// var c = 1
+// while (c <= 3){
+//     console.log("hello World")
+//     c++
+// }
+
+
+
+
+// com teste lógico no final (DO...WHILE)
+
+// var c = 1
+// do{
+//     console.log("eu amo pudim")
+//     c++
+// }while(c <=5 )
+
+
+
+
+// com variavel de controle (FOR)
+
