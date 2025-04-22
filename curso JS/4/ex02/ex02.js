f3589eb8f2d2e0fbd36c5cd9179a6eb404371750
Eduardo <ecarllos017@gmail.com>
2025-04-22 19:22:37 -0300

    curso em video/ ex02 (100%)

--- curso JS/4/ex02/ex02.js
+++ curso JS/4/ex02/ex02.js
@@ -1,16 +1,18 @@
 function calcular(){
     let txtn = document.getElementById("txtn")
     let n = Number(txtn.value)
-    let list = []
-    var res = document.getElementById("res")
+    var res = document.getElementById("seltab")
 
     if(n.leght == 0){
         alert("digite algum número")
     } 
     else{
+
+        res.innerHTML = ``
         for(let c = 1; c <= 10; c++){
             let item = document.createElement("option")
             item.text = `${n} x ${c}= ${n * c}`
+            res.appendChild(item)
         }        
     }
 
