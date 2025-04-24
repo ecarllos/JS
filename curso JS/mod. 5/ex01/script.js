c6f90106ce2cbfd88a39b25b9b325493bde2f50d
Eduardo <ecarllos017@gmail.com>
2025-04-24 18:58:58 -0300

    curso em video/ mod.5 - ex01 (function de calcular 95% e a function de adicionar deu erro)

--- curso JS/mod. 5/ex01/script.js
+++ curso JS/mod. 5/ex01/script.js
@@ -1,6 +1,19 @@
+let = seltab = document.getElementById("seltab")
+let txtn = document.getElementById("txtn")
+let num = Number(txtn.value)
+let list = []
 
-let c = 0
-let verif = []
+// let c = 0
+// let verif =[]
+
+// function naLista(n, l){
+//     if(l.indexOf(n) != -1){
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
 
 function adicionar(){
     /* 
@@ -9,25 +22,42 @@ function adicionar(){
     o elemento adicionado é um novo elemento <option> com o valor do input #txtn,
     */
 
-    let = seltab = document.getElementById("seltab")
-    let txtn = document.getElementById("txtn")
-    let num = Number(txtn.value)
-
-    
-
-    if(num.lenght == 0){
+    if(num.lenght == 0 || num >= 10){
         alert("digite um número corretamente")
     } else {
-        // let cont = verif[c]
-        // verif.push(num)
+        // list.push(num)
         let add = document.createElement("option")
         add.text = `Valor ${num} adicionado`
-        seltab.appendChild(add)   
-        // c++       
-    }   
-    // console.log(cont)
+        seltab.appendChild(add)
+    }
+    console.log(add)
+    // num = ''
+    // txtn.focus()
 }
 
-function calcular(){
+// function calcular(){
+//     let res = document.getElementById("res")
+
+//     let total = list.length
+//     res.innerHTML = `Ao todo, temos ${total} números cadastrados`
+
+
+//     let maior_numero = list[total-1]
+//     res.innerHTML = `o maior número cadastrado é ${maior_numero}`
+
+
+//     let menor_numero = list[0]
+//     res.innerHTML = `o menor número cadastrado é ${menor_numero}`
+
+
+//     let soma = 0;
+//     for (let c in list) {
+//         soma += list[c];
+//     }
+//     res.innerHTML = `A soma dos números cadastrados é ${soma}`;
+
+
     
-}
+//     let media = soma / list.length;
+//     res.innerHTML = `A média dos números cadastrados é ${media}`;
+// }
