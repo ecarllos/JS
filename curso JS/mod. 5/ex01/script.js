cffb23334e5058c2ba2bb0935479e3ee56ff6834
Eduardo <ecarllos017@gmail.com>
2025-04-25 00:01:22 -0300

    curso em video/ mod.5 - ex1 (finalizado 99% - falta apenas os comentários no código e uma pequena documentação)

--- curso JS/mod. 5/ex01/script.js
+++ curso JS/mod. 5/ex01/script.js
@@ -1,19 +1,19 @@
 let = seltab = document.getElementById("seltab")
 let txtn = document.getElementById("txtn")
-let num = Number(txtn.value)
+
 let list = []
 
 // let c = 0
 // let verif =[]
 
-// function naLista(n, l){
-//     if(l.indexOf(n) != -1){
-//         return true
-//     }
-//     else{
-//         return false
-//     }
-// }
+function naLista(n, l){
+    if(l.indexOf(n) != -1){
+        return false
+    }
+    else{
+        return true
+    }
+}
 
 function adicionar(){
     /* 
@@ -21,43 +21,51 @@ function adicionar(){
     que é uma tabela com id "seltab" no HTML.
     o elemento adicionado é um novo elemento <option> com o valor do input #txtn,
     */
+    let num = Number(txtn.value)
 
-    if(num.lenght == 0 || num >= 10){
+    if(num.lenght == 0 || num >= 10 ){
         alert("digite um número corretamente")
-    } else {
-        // list.push(num)
+    } 
+    else if( !naLista(num, list)){
+        alert("Esse número já foi cadastrado")
+    } 
+
+    else {
+        list.push(num)
         let add = document.createElement("option")
         add.text = `Valor ${num} adicionado`
         seltab.appendChild(add)
     }
-    console.log(add)
-    // num = ''
-    // txtn.focus()
+    txtn.value = ''
+    txtn.focus()
 }
 
-// function calcular(){
-//     let res = document.getElementById("res")
+function calcular(){
+    let res = document.getElementById("res")
 
-//     let total = list.length
-//     res.innerHTML = `Ao todo, temos ${total} números cadastrados`
+    res.innerHTML = ``
 
-
-//     let maior_numero = list[total-1]
-//     res.innerHTML = `o maior número cadastrado é ${maior_numero}`
+    let total = list.length
+    res.innerHTML += `Ao todo, temos ${total} números cadastrados <br>`
 
 
-//     let menor_numero = list[0]
-//     res.innerHTML = `o menor número cadastrado é ${menor_numero}`
+    let maior_numero = list[total-1]
+    res.innerHTML += `o maior número cadastrado é ${maior_numero} <br>`
 
 
-//     let soma = 0;
-//     for (let c in list) {
-//         soma += list[c];
-//     }
-//     res.innerHTML = `A soma dos números cadastrados é ${soma}`;
+    let menor_numero = list[0]
+    res.innerHTML += `o menor número cadastrado é ${menor_numero}<br>`
+
+
+    let soma = 0;
+    for (let c in list) {
+        soma += list[c];
+    }
+    res.innerHTML += `A soma dos números cadastrados é ${soma}<br>`
 
 
     
-//     let media = soma / list.length;
-//     res.innerHTML = `A média dos números cadastrados é ${media}`;
-// }
+    let media = soma / list.length;
+    res.innerHTML += `A média dos números cadastrados é ${media}`
+}
+
