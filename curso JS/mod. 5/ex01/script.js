5e1b693e8d6ffa1299617fdcd53ba6249e319811
Eduardo <ecarllos017@gmail.com>
2025-04-28 14:37:47 -0300

    curso em video/ mod.5 - ex01 (função limpar finalizada)

--- curso JS/mod. 5/ex01/script.js
+++ curso JS/mod. 5/ex01/script.js
@@ -1,20 +1,20 @@
 let seltab = document.getElementById("seltab") // indica o select pela variável (seltab)
-let txtn = document.getElementById("txtn") // indica o input numérico pela variável (txtn)
-let res = document.getElementById("res") // indica a div onde as respostas serão inseridas
+let txtn = document.getElementById("txtn")    // indica o input numérico pela variável (txtn)
+let res = document.getElementById("res")     // indica a div onde as respostas serão inseridas
 
 let list = [] // cria o array onde os dados serão inseridos e calculados
 
 let add = 0
 function naLista(n, l){ //função que verifica se o número digitado está na lista ou não
-    // n == valor digittado pelo usuário
+     // n == valor digittado pelo usuário
     // l == lista que o valor foi atribuído
     if(l.indexOf(n) != -1){ 
-        // essa função verifica se o valor (n) está inserido na lista (l)
+         // essa função verifica se o valor (n) está inserido na lista (l)
         // se o valor for diferente de (-1) quer dizer que o número não está na lista
         return false
     }
     else{
-        // se n estiver na lista o valor será -1
+         // se n estiver na lista o valor será -1
         // logo ele não poderá ser executado pq já existe esse valor dentro da nossa lista
         return true
     }
@@ -37,15 +37,15 @@ function adicionar(){
 
     else { // passada a estapa de condições o código de implantação de números começa agora
 
-        list.push(num) // adiciona o número digitado a nossa array
+        list.push(num)                          // adiciona o número digitado a nossa array
         add = document.createElement("option") // cria um elemento (option) dentro do nosso select caracterizado pela variável (add)
-        add.text = `Valor ${num} adicionado` // adiciona um texto a variável (add) que será o texto que o usuário vai conseguir olhar
+        add.text = `Valor ${num} adicionado`  // adiciona um texto a variável (add) que será o texto que o usuário vai conseguir olhar
         seltab.appendChild(add)
     }
     txtn.value = '' // faz com que no final do processo de adicionar a caixa de texto fique limpa/vazia
     txtn.focus() 
 
-    res.innerHTML = list
+    // res.innerHTML = list
 }
 
 function calcular(){
@@ -63,12 +63,12 @@ function calcular(){
         por isso que esse código só permite números de 1 a 9 pq se for algum número
         maior o código não ocorrerá normalmente */
 
-        let total = list.length  // conta quantos números estão cadastrados na array [list]
+        let total = list.length           // conta quantos números estão cadastrados na array [list]
         let maior_numero = list[total-1] // calcula o maoir número da array (list) pegando a última posição da lis
-        let menor_numero = list[0] // calcula o menor número pegando o 1° número cadastrado na array, já que a array foi ordanada em ordem crescente
-        let soma = 0; // variável de soma 
+        let menor_numero = list[0]      // calcula o menor número pegando o 1° número cadastrado na array, já que a array foi ordanada em ordem crescente
+        let soma = 0;          // variável de soma 
         for (let c of list) { // a variável (c)  percorrerá toda a array => sua função é adquirir o valor de acordo com a posição da array
-            soma += c // adiciona o valor de (c) na variável (soma) e depois adicionna o novo valor de (c) com o antigo armazenado na variável (soma) 
+            soma += c        // adiciona o valor de (c) na variável (soma) e depois adicionna o novo valor de (c) com o antigo armazenado na variável (soma) 
         }
         let media = soma / list.length // calcula a media dos números cadastrados => pega a variável de soma e divide pelo total de números
 
@@ -85,10 +85,15 @@ function calcular(){
 }
 
 function limpar(){
+    
     list.splice(0, list.length) // apaga todos os elementos da array
 
+    seltab.innerHTML = "" // apaga TODOS os options do select
+    res.innerHTML = ""   // apaga todos os itens que aparecem depois dos números serem enviados e cadastrados
 
-    seltab.removeChild(add)
-    res.innerHTML = " ."
+    
+    let opt = document.createElement("option") 
+    opt.innerHTML = "adicione algum número acima" // adiciona um option no select que foi apagado anteriormente nessa mesma função
+    seltab.appendChild(opt)
 }
 
