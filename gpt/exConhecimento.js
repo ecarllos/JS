9c93e5823e52a7d71abe6233c6c56e373bdd9af9
Eduardo <ecarllos017@gmail.com>
2025-05-07 15:46:27 -0300

    exercício 1 finalizado

--- gpt/exConhecimento.js
+++ gpt/exConhecimento.js
@@ -1,5 +1,14 @@
 // 1. Função que recebe um array de números e retorna um novo array com apenas os números pares.
+let list = [2, 21, 32, 84, 53]
+let Par = []
 
+for (let c in list){
+    if (list[c] % 2 == 0){
+        Par.push(list[c])
+    }
+}
+console.log(list)
+console.log(Par)
 
 // 2. Escreva uma função que conte quantas vezes cada letra aparece em uma string.
 
